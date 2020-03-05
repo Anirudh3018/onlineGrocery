62c098bd314e35ce85e922538f5d18f1fa0ee230
imnidhi <nidhim214@gmail.com>
2020-03-05 14:53:36 +0530

    First commit

---
+++ src/application/CreateUser.java
@@ -0,0 +1,46 @@
+package application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CreateUser {
+
+    @FXML
+    private TextField firstname;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField flatno;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private TextField street;
+
+    @FXML
+    private TextField locality;
+
+    @FXML
+    private Button create;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField lastname;
+
+    @FXML
+    void createaccount() {
+
+    }
+
+//    @FXML
+//    void 030100(){
+//
+//    }
+
+}
---
+++ src/application/LoginPageController.java
@@ -0,0 +1,53 @@
+package application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginPageController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Label l1;
+
+    @FXML
+    private Label l2;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private Button signup;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    void ffffff26() {
+
+    }
+
+    @FXML
+    void login() {
+
+    }
+
+//    @FXML
+//    void 030100() {
+//
+//    }
+
+    @FXML
+    void signup() {
+
+    }
+
+//    @FXML
+//    void 030100(ActionEvent event) {
+//
+//    }
+
+}
