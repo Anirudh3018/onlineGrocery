ed7a334e65773a9845ab7a6bc644f3da46635dba
imnidhi <nidhim214@gmail.com>
2020-03-16 01:17:06 +0530

    Fetching categories and items

--- src/application/Categories.java
+++ src/application/Categories.java
@@ -1,6 +1,10 @@
 package application;
 import java.awt.Insets;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import javafx.scene.layout.HBox;
 import javafx.collections.FXCollections;
@@ -9,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
@@ -17,17 +22,19 @@ import javafx.scene.layout.*;
 
 public class Categories extends ListView<String> implements Initializable {
 	
+	
     @FXML
     private Label heading;
 
     @FXML
     private ListView<String> itemsList;
+   
+ ObservableList<String> listView = FXCollections.observableArrayList();
+ ObservableList<String> categories = FXCollections.observableArrayList();
+ String price;
 
-    @FXML
-    private MenuButton categoryDropDown;
- 
- ObservableList<String> listView = FXCollections.observableArrayList("Nidhi","Anirudh","Ankita","newItem1","newItem2","newItem3");
- 
+ @FXML
+ private ComboBox<String> categoryDropDown;
  static class Cell extends ListCell<String>{
 	 HBox hbox = new HBox(300);
 	 HBox hbox1 = new HBox(20);
@@ -35,23 +42,27 @@ public class Categories extends ListView<String> implements Initializable {
 	 Button delete = new Button("DELETE");
 	 Pane pane = new Pane();
 	 Label label = new Label();
+	 Label label1 = new Label();
 	 
-	 public Cell() {
+	 
+	 public Cell(String price) {
 		 super();
-		 label.setMinWidth(80);
+		 
+		 label.setMinWidth(100);
 		 hbox1.getChildren().addAll(add,delete);
-		 hbox.getChildren().addAll(label,hbox1);
+		 hbox.getChildren().addAll(label,label1,hbox1);
 		 hbox.setHgrow(pane, Priority.ALWAYS);
 		 
 		
 		 
 	 }
 	 public void updateItem(String name,boolean empty) {
-		 super.updateItem(name, empty);
+		 super.updateItem(name,empty);
 		 setText(null);
 		 setGraphic(null);
 		 if(name!=null&&!empty) {
 			 label.setText(name);
+			 
 			 setGraphic(hbox);
 			 }
 	 }
@@ -60,7 +71,61 @@ public class Categories extends ListView<String> implements Initializable {
  }
  public void initialize(URL arg0,ResourceBundle arg1) {
  itemsList.setItems(listView);
- itemsList.setCellFactory(param->new Cell());
+ fetchCategories();
+ fetchFilteredItems();
+ categoryDropDown.getItems().addAll(categories); 
+ itemsList.setCellFactory(param->new Cell(price));
  
 }
+ @FXML
+ void fetchFilteredItems() {
+	System.out.print(categoryDropDown.getValue());
+   fetchItems(categoryDropDown.getSelectionModel().getSelectedIndex()+1);
+ }
+ 
+ @FXML
+ void fetchCategories() {
+try {
+	Class.forName("oracle.jdbc.driver.OracleDriver");
+	Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","nidhi","ilmm2526");
+	String SQL = " select category_name from categories";
+	PreparedStatement ps=con.prepareStatement(SQL);
+	ResultSet rs = ps.executeQuery();
+	while(rs.next()) {
+		categories.add(rs.getString("category_name"));
+		
+	
+	}
+	ps.close();
+	rs.close();
+}
+catch(Exception e){
+	System.out.println("Not Category fetched");
+}
+
+ }
+ 
+ void fetchItems(int index) {
+	 try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","nidhi","ilmm2526");
+			String SQL = "select item_name,brand,price from items where category_id ="+index;
+			PreparedStatement ps=con.prepareStatement(SQL);
+			ResultSet rs = ps.executeQuery();
+			listView.clear();
+			while(rs.next()) {
+				
+				listView.add(rs.getString("item_name"));
+				price = " ";
+				price = rs.getString("price");
+			}
+			System.out.print(listView+price);
+			ps.close();
+			rs.close();
+		}
+		catch(Exception e){
+			System.out.println("Not Category fetched");
+		}
+ }
+ 
 }
